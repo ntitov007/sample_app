66a4d73764f38f85f53111d64c9ded72dcb0c772
Nikolai Titov <ntitov007@gmail.com>
2015-01-31 13:03:16 +0300

    Finish layout and routes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,8 @@ require 'rspec/rails'
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.include Rails.application.routes.url_helpers
+  config.include ApplicationHelper
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
