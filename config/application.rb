90c596c743f1abf0f5552edb5eb3ed0c2403a457
Nikolai Titov <ntitov007@gmail.com>
2015-02-01 12:10:11 +0300

    Make a basic User model (Including secure passwords)

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module SampleApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    I18n.enforce_available_locales = true
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,7 @@ require 'rspec/rails'
 RSpec.configure do |config|
   config.include Rails.application.routes.url_helpers
   config.include ApplicationHelper
+  ActiveRecord::Migration.maintain_test_schema!
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
